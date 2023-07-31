34f07c8c636ba76f4517b8370f6e28c68b36adeb
manhnv2003 <106580260+manhnv2003@users.noreply.github.com>
2023-07-31 21:53:14 +0700

    Update ExampleUnitTest.java
    new line in newbranch

--- app/src/test/java/com/kunminx/puremusic/ExampleUnitTest.java
+++ app/src/test/java/com/kunminx/puremusic/ExampleUnitTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 /**
+* new line here
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
